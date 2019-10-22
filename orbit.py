b9da9bbd59be4c50fb81f2aca6dfb8f76f919c32
John Trager <johntrager0226@gmail.com>
2019-10-22 18:52:11 -0400

    init

---
+++ orbit.py
@@ -0,0 +1,71 @@
+'''
+Notes:
+-h needs to be in meters for v0 calc
+-full orbit not calibrated
+
+'''
+
+
+from vpython import *
+from math import pi
+import csv
+
+#intialize data collect
+#data_vt = []
+
+#intialize volocity plot
+Vx_plot = gcurve(color=color.red)
+Vy_plot = gcurve(color=color.blue)
+
+M = 6.0e24 #mass of Earth
+R = 6.378e6 #radius of Earth, in meters
+G = 6.67e-11 #gravity constant in Nm^2/kg^2
+
+h = 3.6e7 #heigt of orbit of rocket in km
+m = 100 #mass of rocket for momentum calc
+
+Earth = sphere(pos=vector(0,0,0), radius=R, color=color.blue)
+#intial position of rocket
+rocket_pos_init = vector(Earth.pos.x + R + h,Earth.pos.y,Earth.pos.z) #remeber that for regular orbit put h or R in place of (x,-,-)
+#rocket object
+rocket = sphere(pos=rocket_pos_init, radius= R / 5, color=color.white,
+                make_trail=True, trail_type="points", interval=10)
+
+v_esc = sqrt((2*G*M) / R)
+
+#intial velocity (gravity*Mass of earth) / (radius of earth + rocket's height) 
+                                            #total distance to rocket from Earth's center
+v0 = sqrt((G*M)/(R+h))   #sqrt((G*M)/(R+h))  or   1.1*v_esc 
+
+v = vector(0,v0,0)
+#time of full orbit in hours from -side not maybe change to equation with the height of the orbit and have equation calc
+T_circ = (2*pi*(R+h)) / v0
+dt = 0.001*T_circ
+
+#intialzie force, momentum-p, and set vector radius-r?r-hat vector
+F = vector(0,0,0)
+p = m*v
+r = rocket.pos - Earth.pos
+
+t=0 #time
+
+while t < 15 * T_circ:
+    rate(1000)
+    #Force pulling rocket to earth
+    F = -(G * M * m)/((R+mag(r)**2))*norm(r)
+    p = p + (F*dt)
+    v = p/m
+        #plot volicity over time
+    Vx_plot.plot((t,v.x))
+    Vy_plot.plot((t,v.y))
+    #update position
+    rocket.pos = rocket.pos + v*dt
+    r = rocket.pos - Earth.pos
+    t += dt
+
+    #collect data
+    #data_vt += [[t,v.y]]
+
+#with open('orbit_1.csv', 'w') as f:
+#    f.write(str(data_vt))
+#print(data_vt)
---
+++ take_off.py
@@ -0,0 +1,74 @@
+'''
+Rocket Launch Sim
+'''
+
+
+from vpython import *
+from math import pi
+import csv
+
+#intialize data collect
+#data_vt = []
+
+#intialize velocity plot
+Vx_plot = gcurve(color=color.red)
+Vy_plot = gcurve(color=color.blue)
+
+M = 6.0e24 #mass of Earth
+R = 6.378e6 #radius of Earth, in meters
+G = 6.67e-11 #gravity constant in Nm^2/kg^2
+
+h = 3.6e7 #heigt of orbit of rocket in km
+m = 100 #mass of rocket for momentum calc
+
+Earth = sphere(pos=vector(0,0,0), radius=R, color=color.blue)
+#intial position of rocket
+rocket_pos_init = vector(Earth.pos.x + R,Earth.pos.y,Earth.pos.z) #remeber that for regular orbit put h or R in place of (x,-,-)
+#rocket object
+rocket = sphere(pos=rocket_pos_init, radius= R / 5, color=color.white,
+                make_trail=True, trail_type="points", interval=10)
+
+v_esc = sqrt((2*G*M) / R)
+
+#intial velocity (gravity*Mass of earth) / (radius of earth + rocket's height) 
+                                            #total distance to rocket from Earth's center
+v0 = 0.7*v_esc   #sqrt((G*M)/(R+h))  or   1.1*v_esc 
+
+v = vector(0,v0,0)
+#time of full orbit in hours from -side not maybe change to equation with the height of the orbit and have equation calc
+T_circ = (2*pi*(R+h)) / v0
+dt = 0.001*T_circ
+
+#intialzie force, momentum-p, and set vector radius-r?r-hat vector
+F = vector(0,0,0)
+p = m*v
+r = rocket.pos - Earth.pos
+
+t=0 #time
+#-+error for object detection of earth
+e = 1e4
+
+while t < 1000 * T_circ:
+    rate(1000)
+    #Force pulling rocket to earth
+    F = -((G * M * m)/((R+mag(r))**2))*norm(r)
+    p = p + (F*dt)
+    v = p/m
+    #plot velocity over time
+    Vx_plot.plot((t,mag(r)))
+    Vy_plot.plot((t,v.y))
+    #update position
+    rocket.pos = rocket.pos + v*dt
+    r = rocket.pos - Earth.pos
+    t += dt
+    #print("r-hat: {} r: {} t: {}".format(mag(r),r,t))
+    
+    if (t > dt and mag(r) < R + e):
+        break
+    
+        
+print("---DONE--- \nt: {} \nR: {} \nmag(r): {}".format(t,R,mag(r)))
+
+    
+    
+    
